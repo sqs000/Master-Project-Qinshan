09748fb3406b1d814e1d5b07c1ba32f272086065
sqs000 <1585250244@qq.com>
2023-11-20 20:34:53 +0100

    Update coverType_experiment.py

--- coverType_experiment.py
+++ coverType_experiment.py
@@ -6,44 +6,40 @@ from data import getForestCoverTypeDataset
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import nevergrad as ng
-import numpy as np
 
 
 # Read Forest Cover Type dataset
 train_dataset, test_dataset, X_train, y_train, X_test, y_test = getForestCoverTypeDataset(file_path="C:/Users/15852/Desktop/ThirdS/MasterProject/covertype/covtype.data")
-
-# train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-# test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
-
-# # sgd optimization
-# num_epochs = 300
-# sgd_learning_rate = 0.001
-# criterion = nn.CrossEntropyLoss()
-# sgd_model = FNN(54, 20, 7)
-# sgd_optimizer = optim.SGD(sgd_model.parameters(), lr=sgd_learning_rate)
-
-# for epoch in range(num_epochs):
-#     sgd_model.train()
-#     epoch_loss = 0.0
-#     for inputs, labels in train_loader:
-#         sgd_optimizer.zero_grad()
-#         outputs = sgd_model(inputs)
-#         loss = criterion(outputs, labels)
-#         epoch_loss += loss.item()
-#         loss.backward()
-#         sgd_optimizer.step()
-#     print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss/len(train_loader)}')
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
 
-# print(f'Parameters: {[param.data.tolist()[0] for param in sgd_model.parameters()][0]}, Loss: {epoch_loss.item()}')
+# SGD optimization
+num_epochs = 30
+sgd_learning_rate = 0.01
+criterion = nn.CrossEntropyLoss()
+sgd_model = FNN(54, 54, 7)
+sgd_optimizer = optim.SGD(sgd_model.parameters(), lr=sgd_learning_rate)
+
+print("Start training using sgd")
+for epoch in range(num_epochs):
+    sgd_model.train()
+    epoch_loss = 0.0
+    for inputs, labels in train_loader:
+        sgd_optimizer.zero_grad()
+        outputs = sgd_model(inputs)
+        loss = criterion(outputs, labels)
+        epoch_loss += loss.item()
+        loss.backward()
+        sgd_optimizer.step()
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss/len(train_loader)}')
 
 
-# cma optimization
-cma_model = FNN(54, 20, 7)
+# CMA optimization
+cma_model = FNN(54, 54, 7)
 num_parameters = sum(p.numel() for p in cma_model.parameters())
 cma_optimizer = ng.optimizers.CMA(parametrization=num_parameters, budget=5000)
 
-def objective_f(x):
+def objective_f_train(x):
     # Split the 1D array into parts corresponding to the model parameters
     new_params = torch.split(torch.tensor(x), [p.numel() for p in cma_model.parameters()])
     with torch.no_grad():
@@ -54,6 +50,29 @@ def objective_f(x):
     loss = criterion(predicted_y, y_train)
     return loss.item()
 
-recommendation = cma_optimizer.minimize(objective_f)
-print(f'Parameters: {recommendation.value}, Loss: {objective_f(recommendation.value)}')
+print("Start training using cma")
+recommendation = cma_optimizer.minimize(objective_f_train)
+print(f'Parameters: {recommendation.value}, Loss: {objective_f_train(recommendation.value)}')
 
+
+print("Start the evaluation of two models")
+# Test sgd_model
+sgd_model.eval()
+with torch.no_grad():
+    test_outputs = sgd_model(X_test)
+    _, predicted = torch.max(test_outputs, 1)
+    accuracy = (predicted == y_test).sum().item() / len(y_test)
+    print(f"Accuracy of the model optimized by SGD: {accuracy * 100:.2f}%")
+
+# Test cma_model
+def objective_f_test(x):
+    new_params = torch.split(torch.tensor(x), [p.numel() for p in cma_model.parameters()])
+    with torch.no_grad():
+        for param, new_param_value in zip(cma_model.parameters(), new_params):
+            param.data.copy_(new_param_value.reshape(param.data.shape))
+    test_outputs = cma_model(X_test)
+    _, predicted = torch.max(test_outputs, 1)
+    accuracy = (predicted == y_test).sum().item() / len(y_test)
+    print(f"Accuracy of the model optimized by CMA: {accuracy * 100:.2f}%")
+
+objective_f_test(recommendation.value)
