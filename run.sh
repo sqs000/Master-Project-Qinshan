bcb06bb8dc51ab64c14e412c9956b4faef9952e3
sqs000 <1585250244@qq.com>
2024-03-14 14:39:49 +0100

    new bash file

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Specify the number of repetitions
+repetitions=10
+
+# Specify the BBOB function numbers in an array
+functions=(3 7 13 16 22)
+# Specify the algorithms
+algorithms=(Adam SGD GA GA_sharing GA_dynamic)
+
+# Loop through the functions
+for f in "${functions[@]}"
+do
+    # Loop through the algorithms
+    for algo in "${algorithms[@]}"
+    do
+        # Repeat the run for the specified number of repetitions
+        for ((rep=1; rep<=$repetitions; rep++))
+        do
+            if [ "$algo" == "Adam" ]; then
+                python run.py $f $algo 1000000 -l 0.00001 -b 64 -i $rep
+            elif [ "$algo" == "SGD" ]; then
+                python run.py $f $algo 1000000 -l 0.00001 -b 64 -i $rep
+            elif [ "$algo" == "GA" ]; then
+                python run.py $f $algo 1000000 -p 1000 -m 0.04 -i $rep
+            elif [ "$algo" == "GA_sharing" ]; then
+                python run.py $f $algo 1000000 -p 1000 -m 0.04 -r 5 -i $rep
+            elif [ "$algo" == "GA_dynamic" ]; then
+                python run.py $f $algo 1000000 -p 1000 -m 0.04 -r 5 -n 50 -i $rep
+            fi
+        done
+    done    
+done
