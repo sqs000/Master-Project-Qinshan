46d4aa31b16a49bf13529674b6f45727d2a5c77c
sqs000 <1585250244@qq.com>
2024-04-04 20:06:33 +0200

    add visualization from artur-deluca/landscapeviz

---
+++ visualize/Visualize.py
@@ -0,0 +1,40 @@
+import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+import landscapeviz
+import cocoex as ex
+import numpy as np
+
+# 1. define model
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(2,)),
+	tf.keras.layers.Dense(50, activation=tf.nn.relu),
+	tf.keras.layers.Dense(20, activation=tf.nn.relu),
+	tf.keras.layers.Dense(1)
+])
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.00005)
+model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mae'])
+
+# 2. get data
+suite = ex.Suite(suite_name="bbob", suite_instance='', suite_options='')
+problem = suite.get_problem_by_function_dimension_instance(function=3, dimension=2, instance=1)
+data_size = 5000
+x = []
+y = []
+for i in range(data_size):
+	input = np.random.uniform(low=-5, high=5, size=2)
+	output = problem(input)
+	x.append(input)
+	y.append(output)
+X_train, y_train = np.array(x), np.array(y)
+scaler_x = MinMaxScaler(feature_range=(-1,+1)).fit(X_train)
+X_train = scaler_x.transform(X_train)
+
+# 3. train model
+model.fit(X_train, y_train, batch_size=32, epochs=200, verbose=1)
+
+# 4. build mesh and plot
+landscapeviz.build_mesh(model, (X_train, y_train), grid_length=100, verbose=1)
+landscapeviz.plot_contour(key="mean_squared_error")
+landscapeviz.plot_3d(key="mean_squared_error")
---
+++ visualize/landscapeviz/utils.py
@@ -0,0 +1,133 @@
+import logging
+import gc
+import os
+
+import h5py
+import numpy as np
+import tensorflow as tf
+from sklearn.decomposition import PCA
+
+from .trajectory import load_weights, weight_encoder
+
+
+def get_vectors(model, seed=None, trajectory=None):
+
+    np.random.seed(seed)
+    vector_x, vector_y = list(), list()
+    weights = model.get_weights()
+
+    if trajectory:
+        # this has to be re-written
+        load_weights(model, trajectory)
+        file_path = os.path.join(trajectory, ".trajectory", "model_weights.hdf5")
+
+        with h5py.File(file_path, "r+") as f:
+            differences = list()
+            trajectory = np.array(f["weights"])
+            for i in range(0, len(trajectory) - 1):
+                differences.append(trajectory[i] - trajectory[-1])
+
+            pca = PCA(n_components=2)
+            pca.fit(np.array(differences))
+            f["X"], f["Y"] = pca.transform(np.array(differences)).T
+
+        vector_x = weight_encoder(model, pca.components_[0])
+        vector_y = weight_encoder(model, pca.components_[1])
+
+        return weights, vector_x, vector_y
+
+    else:
+        cast = np.array([1]).T
+        for layer in weights:
+            # set standard normal parameters
+            # filter-wise normalization
+            k = len(layer.shape) - 1
+            d = np.random.multivariate_normal([0], np.eye(1), layer.shape).reshape(
+                layer.shape
+            )
+            dist_x = (
+                d / (1e-10 + cast * np.linalg.norm(d, axis=k))[:, np.newaxis]
+            ).reshape(d.shape)
+
+            vector_x.append(
+                (
+                    dist_x * (cast * np.linalg.norm(layer, axis=k))[:, np.newaxis]
+                ).reshape(d.shape)
+            )
+
+            d = np.random.multivariate_normal([0], np.eye(1), layer.shape).reshape(
+                layer.shape
+            )
+            dist_y = (
+                d / (1e-10 + cast * np.linalg.norm(d, axis=k))[:, np.newaxis]
+            ).reshape(d.shape)
+
+            vector_y.append(
+                (
+                    dist_y * (cast * np.linalg.norm(layer, axis=k))[:, np.newaxis]
+                ).reshape(d.shape)
+            )
+
+        return weights, vector_x, vector_y
+
+
+def _obj_fn(model, data, solution):
+
+    old_weights = model.get_weights()
+    model.set_weights(solution)
+    value = model.evaluate(data[0], data[1], verbose=0)
+    model.set_weights(old_weights)
+
+    return value
+
+
+def build_mesh(
+    model,
+    data,
+    grid_length,
+    extension=1,
+    filename="meshfile",
+    verbose=True,
+    seed=None,
+    trajectory=None,
+):
+
+    logging.basicConfig(level=logging.INFO)
+
+    z_keys = model.metrics_names
+    z_keys[0] = model.loss
+    Z = list()
+
+    # get vectors and set spacing
+    origin, vector_x, vector_y = get_vectors(model, seed=seed, trajectory=trajectory)
+    space = np.linspace(-extension, extension, grid_length)
+
+    X, Y = np.meshgrid(space, space)
+
+    for i in range(grid_length):
+        if verbose:
+            logging.info("line {} out of {}".format(i, grid_length))
+
+        for j in range(grid_length):
+            solution = [
+                origin[x] + X[i][j] * vector_x[x] + Y[i][j] * vector_y[x]
+                for x in range(len(origin))
+            ]
+
+            Z.append(_obj_fn(model, data, solution))
+
+    Z = np.array(Z)
+    os.makedirs("./files", exist_ok=True)
+
+    with h5py.File("./files/{}.hdf5".format(filename), "w") as f:
+
+        f["space"] = space
+        original_results = _obj_fn(model, data, origin)
+
+        for i, metric in enumerate(z_keys):
+            f["original_" + metric] = original_results[i]
+            f[metric] = Z[:, i].reshape(X.shape)
+        f.close()
+
+    del Z
+    gc.collect()
